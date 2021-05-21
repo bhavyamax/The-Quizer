dd83ab04098bed59ad20f176379cb5fe5fe0371c
bhavyamax <bhavyamax@gmail.com>
2021-05-21 16:01:58 -0400

    Redo

---
+++ The Quizer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace The_Quizer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
