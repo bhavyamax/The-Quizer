9b46ffd1019e8d1a61fd3342dda2140dc6222b6a
bhavyamax <bhavyamax@gmail.com>
2021-05-23 18:31:54 -0400

    List All admin Users

--- The Quizer/Controllers/AdminManController.cs
+++ The Quizer/Controllers/AdminManController.cs
@@ -6,21 +6,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using The_Quizer.Models;
+using The_Quizer.ViewModels;
 
 namespace The_Quizer.Controllers
 {
-    [Authorize(Roles ="Admin")]
     public class AdminManController : Controller
     {
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
 
-        public AdminManController(UserManager<ApplicationUser> userManager)
+        public AdminManController(UserManager<ApplicationUser> userManager,
+                                    RoleManager<IdentityRole> roleManager)
         {
             this.userManager = userManager;
+            this.roleManager = roleManager;
         }
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task< IActionResult> ListUsers()
+        {
+            var users = await userManager.GetUsersInRoleAsync("Admin");
+            return View(users);
+        }
+        
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AdminRegisterViewModel adminRegisterViewModel = new AdminRegisterViewModel
+            {
+                Password = "Admin@1",
+                UserRole = roleManager.Roles.Select(a => new { a.Name, isSelected = false }).ToDictionary(a=>a.Name,(a=>a.Name=="Admin"))
+            };
+            return View();
+        }
+
+
+
     }
 }
