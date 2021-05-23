967e8804a6c15492de92d0a889ee9bfd48ed8d79
bhavyamax <bhavyamax@gmail.com>
2021-05-23 02:38:13 -0400

    Create AdminManController.cs

---
+++ The Quizer/Controllers/AdminManController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using The_Quizer.Models;
+
+namespace The_Quizer.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class AdminManController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AdminManController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
