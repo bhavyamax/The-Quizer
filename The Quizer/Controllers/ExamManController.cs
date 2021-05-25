66cb845027c302f2d0088a5978aa75c12a7b6ffd
bhavyamax <bhavyamax@gmail.com>
2021-05-24 22:46:14 -0400

    Added the auto generated Controller for exams

--- The Quizer/Controllers/ExamManController.cs
+++ The Quizer/Controllers/ExamManController.cs
@@ -1,16 +1,153 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using The_Quizer.Data;
+using The_Quizer.Models;
 
 namespace The_Quizer.Controllers
 {
     public class ExamManController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDBContext _context;
+
+        public ExamManController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ExamMan
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Exams.ToListAsync());
+        }
+
+        // GET: ExamMan/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exam = await _context.Exams
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            return View(exam);
+        }
+
+        // GET: ExamMan/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: ExamMan/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Status")] Exam exam)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(exam);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(exam);
+        }
+
+        // GET: ExamMan/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exam = await _context.Exams.FindAsync(id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+            return View(exam);
+        }
+
+        // POST: ExamMan/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Status")] Exam exam)
+        {
+            if (id != exam.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(exam);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExamExists(exam.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(exam);
+        }
+
+        // GET: ExamMan/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exam = await _context.Exams
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (exam == null)
+            {
+                return NotFound();
+            }
+
+            return View(exam);
+        }
+
+        // POST: ExamMan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var exam = await _context.Exams.FindAsync(id);
+            _context.Exams.Remove(exam);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExamExists(int id)
+        {
+            return _context.Exams.Any(e => e.Id == id);
         }
     }
 }
