4c9e1af84fcddcfd82c746771b96fb2d0f647165
bhavyamax <bhavyamax@gmail.com>
2021-05-24 22:15:13 -0400

    Create SQLUserExamRepository.cs

---
+++ The Quizer/Models/SQLUserExamRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_Quizer.Models
+{
+    public class SQLUserExamRepository : IUserExamRepository
+    {
+        public UserExam AssignExam(int userid, int examD)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserExam UpdateExamScore(UserExam userExam)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
