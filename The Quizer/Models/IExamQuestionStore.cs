1e5282a74a34ab6b10fec65b4194b87fa93ab4ca
bhavyamax <bhavyamax@gmail.com>
2021-06-05 09:12:29 -0400

    Update the Store for questions

--- The Quizer/Models/IExamQuestionStore.cs
+++ The Quizer/Models/IExamQuestionStore.cs
@@ -1,45 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace The_Quizer.Models
 {
     public interface IExamQuestionStore
     {
-        /// <summary>
-        ///     Create a new examQuestion
-        /// </summary>
-        /// <param name="examQuestion"></param>
-        /// <returns></returns>
-        Task CreateAsync(ExamQuestion examQuestion);
-
-        /// <summary>
-        ///     Update a examQuestion
-        /// </summary>
-        /// <param name="examQuestion"></param>
-        /// <returns></returns>
-        Task UpdateAsync(ExamQuestion examQuestion);
-
-        /// <summary>
-        ///     Delete a examQuestion
-        /// </summary>
-        /// <param name="examQuestion"></param>
-        /// <returns></returns>
+        Task<string> CreateAsync(ExamQuestion examQuestion);
         Task DeleteAsync(ExamQuestion examQuestion);
-
-        /// <summary>
-        ///     Find a examQuestion by id
-        /// </summary>
-        /// <param name="examQuestionId"></param>
-        /// <returns></returns>
         Task<ExamQuestion> FindByIdAsync(string examQuestionId);
-
-        /// <summary>
-        ///     Find a examQuestion by name
-        /// </summary>
-        /// <param name="roleName"></param>
-        /// <returns></returns>
-        Task<ExamQuestion> FindByNameAsync(string roleName);
+        Task<ExamQuestion> FindByTitleAsync(string title);
+        Task<List<ExamQuestion>> GetAllAsync();
+        Task<List<ExamQuestion>> GetAllForExamAsync(string examId);
+        Task UpdateAsync(ExamQuestion examQuestion);
     }
 }
--- The Quizer/Models/SQLExamQuestionStore.cs
+++ The Quizer/Models/SQLExamQuestionStore.cs
@@ -1,35 +1,82 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using The_Quizer.Data;
 
 namespace The_Quizer.Models
 {
     public class SQLExamQuestionStore : IExamQuestionStore
     {
-        public Task CreateAsync(ExamQuestion examQuestion)
+        //IExamQuestionStore
+
+        public AppDBContext Context { get; private set; }
+        public bool AutoSaveChanges { get; set; }
+
+        public async Task<string> CreateAsync(ExamQuestion examQuestion)
+        {
+            if (examQuestion == null)
+            {
+                throw new ArgumentNullException("examQuestion");
+            }
+            else if (await Context.Exams.AnyAsync(a => a.Id == examQuestion.Exam_id))
+            {
+                await Context.AddAsync<ExamQuestion>(examQuestion);
+            }
+            await SaveChanges();
+            return examQuestion.ID;
+        }
+
+        public async Task DeleteAsync(ExamQuestion examQuestion)
+        {
+            Context.Remove(examQuestion);
+            await SaveChanges();
+        }
+
+        public async Task<ExamQuestion> FindByIdAsync(string examQuestionId)
+        {
+            var examQues = await Context.ExamQuestions
+                .FirstOrDefaultAsync(m => m.ID == examQuestionId);
+            return examQues;
+        }
+
+        public Task<ExamQuestion> FindByTitleAsync(string title)
         {
             throw new NotImplementedException();
         }
 
-        public Task DeleteAsync(ExamQuestion examQuestion)
+        public async Task<List<ExamQuestion>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var examQuess = from exam in Context.ExamQuestions
+                            select exam;
+            return await examQuess.ToListAsync();
         }
 
-        public Task<ExamQuestion> FindByIdAsync(string examQuestionId)
+        public async Task<List<ExamQuestion>> GetAllForExamAsync(string examId)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(examId))
+            {
+                throw new ArgumentNullException("examId");
+            }
+            var examQuess = from ques in Context.ExamQuestions
+                            where ques.Exam_id == examId
+                            select ques;
+            return await examQuess.ToListAsync();
         }
 
-        public Task<ExamQuestion> FindByNameAsync(string roleName)
+        public async Task UpdateAsync(ExamQuestion examQuestion)
         {
-            throw new NotImplementedException();
+            Context.Update(examQuestion);
+            await Context.SaveChangesAsync();
         }
 
-        public Task UpdateAsync(ExamQuestion examQuestion)
+        private async Task SaveChanges()
         {
-            throw new NotImplementedException();
+            if (AutoSaveChanges)
+            {
+                await Context.SaveChangesAsync();
+            }
         }
     }
 }
