f51af79220b4ac94c4235fae31de94c740ae964d
bhavyamax <bhavyamax@gmail.com>
2021-06-07 06:58:23 -0400

    add model view for teacher exam man

---
+++ The Quizer/ViewModels/TeacherExamMan/ExamDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_Quizer.ViewModels.TeacherExamMan
+{
+    public class ExamDetailsViewModel
+    {
+    }
+}
