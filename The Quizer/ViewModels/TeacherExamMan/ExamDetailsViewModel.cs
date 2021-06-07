6643d7aed58eeb80b764a5856a0c1db2fdc99b9b
bhavyamax <bhavyamax@gmail.com>
2021-06-07 07:24:58 -0400

    Update ExamDetailsViewModel.cs

--- The Quizer/ViewModels/TeacherExamMan/ExamDetailsViewModel.cs
+++ The Quizer/ViewModels/TeacherExamMan/ExamDetailsViewModel.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using The_Quizer.Models;
 
 namespace The_Quizer.ViewModels.TeacherExamMan
 {
     public class ExamDetailsViewModel
     {
+        public Exam Exam { get; set; }
+        public IEnumerable<ExamQuestion> examQuestions { get; set; }
     }
 }
