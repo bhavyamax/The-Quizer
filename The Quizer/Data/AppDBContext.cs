e851242b2422b22e15a14ce7cae94bf4a065e2ba
bhavyamax <bhavyamax@gmail.com>
2021-06-12 13:58:40 -0400

    Course table name fix

--- The Quizer/Data/AppDBContext.cs
+++ The Quizer/Data/AppDBContext.cs
@@ -25,7 +25,7 @@ namespace The_Quizer.Data
         }
 
         public DbSet<Exam> Exams { get; set; }
-        public DbSet<Course> Coursess { get; set; }
+        public DbSet<Course> Courses { get; set; }
         public DbSet<ExamQuestion> ExamQuestions { get; set; }
         public DbSet<QuestionAnswer> QuestionAnswers { get; set; }
         public DbSet<UserExam> UserExams { get; set; }
---
+++ The Quizer/Migrations/20210612175717_AddCoursesFix.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace The_Quizer.Migrations
+{
+    public partial class AddCoursesFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserCourses_Coursess_Course_id",
+                table: "UserCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserExams_Coursess_CourseId",
+                table: "UserExams");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Coursess",
+                table: "Coursess");
+
+            migrationBuilder.RenameTable(
+                name: "Coursess",
+                newName: "Courses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Courses",
+                table: "Courses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserCourses_Courses_Course_id",
+                table: "UserCourses",
+                column: "Course_id",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserExams_Courses_CourseId",
+                table: "UserExams",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserCourses_Courses_Course_id",
+                table: "UserCourses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserExams_Courses_CourseId",
+                table: "UserExams");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Courses",
+                table: "Courses");
+
+            migrationBuilder.RenameTable(
+                name: "Courses",
+                newName: "Coursess");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Coursess",
+                table: "Coursess",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserCourses_Coursess_Course_id",
+                table: "UserCourses",
+                column: "Course_id",
+                principalTable: "Coursess",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserExams_Coursess_CourseId",
+                table: "UserExams",
+                column: "CourseId",
+                principalTable: "Coursess",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
