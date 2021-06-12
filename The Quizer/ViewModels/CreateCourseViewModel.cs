a2d26da888afcc4910ad06a437a0fab65b22628d
bhavyamax <bhavyamax@gmail.com>
2021-06-12 15:08:26 -0400

    Create CreateCourseViewModel.cs

---
+++ The Quizer/ViewModels/CreateCourseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using The_Quizer.Models;
+
+namespace The_Quizer.ViewModels
+{
+    public class CreateCourseViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public CourseStatus Status { get; set; }
+        [Required]
+        [Display(Name ="Teacher")]
+        public string TeacherId{ get; set; }
+
+        public ICollection<ApplicationUser> Teachers{ get; set; }
+    }
+}
