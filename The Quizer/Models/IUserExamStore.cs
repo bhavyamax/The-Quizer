4a7a8f56794ec36133dee4d67fabc8ef457d4050
bhavyamax <bhavyamax@gmail.com>
2021-06-12 12:54:15 -0400

    Update IUserExamStore.cs

--- The Quizer/Models/IUserExamStore.cs
+++ The Quizer/Models/IUserExamStore.cs
@@ -5,7 +5,11 @@ namespace The_Quizer.Models
     public interface IUserExamStore
     {
         Task AddUserExamScoreAsync(UserExam userExam);
+        Task<UserExam> GetUserExamRecordAsync(string userId, string examId);
         Task<Exam> GetExamResultsAsync(string examId);
         Task<UserExam> GetUserResultsAsync(string userId);
+        Task<UserExam> SetUserRetestAsync(UserExam userExam);
+        Task<UserExam> AssingUserToExamAsync(UserExam userExam);
+        Task RemoveUserFromExamAsync(UserExam userExam);
     }
 }
