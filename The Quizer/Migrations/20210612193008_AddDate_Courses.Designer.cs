1172d00a8b5ec9ef3528220be7339b0e51227a3e
bhavyamax <bhavyamax@gmail.com>
2021-06-12 15:31:12 -0400

    Ad dated to courses

--- The Quizer/Migrations/20210612193008_AddDate_Courses.Designer.cs
+++ The Quizer/Migrations/20210612193008_AddDate_Courses.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using The_Quizer.Data;
 
 namespace The_Quizer.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210612193008_AddDate_Courses")]
+    partial class AddDate_Courses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
