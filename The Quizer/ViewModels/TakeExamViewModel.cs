3c4e268247b8ca90fead1679dc486f80a64c44d9
bhavyamax <bhavyamax@gmail.com>
2021-06-14 12:06:41 -0400

    Update TakeExamViewModel.cs

--- The Quizer/ViewModels/TakeExamViewModel.cs
+++ The Quizer/ViewModels/TakeExamViewModel.cs
@@ -8,25 +8,30 @@ namespace The_Quizer.ViewModels
 {
     public class TakeExamViewModel
     {
+        public TakeExamViewModel()
+        {
+            Questions = new();
+        }
         public string examId { get; set; }
         public string Title { get; set; }
-        public List<TakeExamQuesViewModel> Questions
+        public List<TakeExamQuesViewModel> Questions{get; set;}
+    }
+    public class TakeExamQuesViewModel
+    {
+        public TakeExamQuesViewModel()
         {
-            get; set;
+            Answers = new();
         }
-
-        public class TakeExamQuesViewModel
-        {
-            public string quesId { get; set; }
-            public string question { get; set; }
-            public QuestionType quesType { get; set; }
-            public List<TakeExamAnsViewModel> Answers { get; set; }
-        }
-        public class TakeExamAnsViewModel
-        {
-            public string ansId { get; set; }
-            public string ans { get; set; }
-            public bool isSelected { get; set; }
-        }
+        public string quesId { get; set; }
+        public string question { get; set; }
+        public int points { get; set; }
+        public QuestionType quesType { get; set; }
+        public List<TakeExamAnsViewModel> Answers { get; set; }
+    }
+    public class TakeExamAnsViewModel
+    {
+        public string ansId { get; set; }
+        public string ans { get; set; }
+        public bool isSelected { get; set; }
     }
 }
