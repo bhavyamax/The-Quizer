ec77d98508a01c91f39c90c323be6493ae7c2bc1
bhavyamax <bhavyamax@gmail.com>
2021-06-14 11:33:31 -0400

    Create TakeExamViewModel.cs

---
+++ The Quizer/ViewModels/TakeExamViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using The_Quizer.Models;
+
+namespace The_Quizer.ViewModels
+{
+    public class TakeExamViewModel
+    {
+        public string examId { get; set; }
+        public string Title { get; set; }
+        public List<TakeExamQuesViewModel> Questions
+        {
+            get; set;
+        }
+
+        public class TakeExamQuesViewModel
+        {
+            public string quesId { get; set; }
+            public string question { get; set; }
+            public QuestionType quesType { get; set; }
+            public List<TakeExamAnsViewModel> Answers { get; set; }
+        }
+        public class TakeExamAnsViewModel
+        {
+            public string ansId { get; set; }
+            public string ans { get; set; }
+            public bool isSelected { get; set; }
+        }
+    }
+}
