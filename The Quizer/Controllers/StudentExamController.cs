2e0a4bc936bcc1749dca30e4e49f98601c96c3fb
bhavyamax <bhavyamax@gmail.com>
2021-06-14 09:13:27 -0400

    Create StudentExamController.cs

---
+++ The Quizer/Controllers/StudentExamController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using The_Quizer.Models;
+
+namespace The_Quizer.Controllers
+{
+    public class StudentExamController : Controller
+    {
+        private readonly IUserExamStore userExamStore;
+
+        public StudentExamController(IUserExamStore userExamStore)
+        {
+            this.userExamStore = userExamStore;
+        }
+        public async Task<IActionResult> Index()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var model =await userExamStore.GetUserExamsAsync(userId);
+            ViewData["Title"] = "Student Home";
+            return View(model);
+        }
+    }
+}
